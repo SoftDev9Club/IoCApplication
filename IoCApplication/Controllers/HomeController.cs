7c1b42a9f818691f5c4c508e2baafb8382124a04
SoftDev9Club <WLee121@Gmail.com>
2017-09-11 23:08:55 -0700

    test

--- IoCApplication/Controllers/HomeController.cs
+++ IoCApplication/Controllers/HomeController.cs
@@ -14,8 +14,9 @@ namespace IoCApplication.Controllers
     {
         private IService1 s1;
         private IService2 s2;
-
-       public  HomeController(IService1 s1, IService2 s2)
+        Container container = new Container();
+       
+        public HomeController(IService1 s1, IService2 s2)
         {
             this.s1 = s1;
             this.s2 = s2;
@@ -23,17 +24,12 @@ namespace IoCApplication.Controllers
 
         public ActionResult Index()
         {
-            // Create the container object
-            Container container = new Container();
             // Register all the dependencies
             container.Register<IService1, IoCService1>(IoCApplication.Models.CycleTimeOptions.transient);
             container.Register<IService2, IoCService2>(IoCApplication.Models.CycleTimeOptions.singleton);
 
-            //// Prepare the first ResolveService object and do the ResolveService operation
-            //ResolveService ResolveService1 = new ResolveService(container);            
-            //// Prepare the second ResolveService object and do the ResolveService operation
-            //ResolveService ResolveService2 = new ResolveService(container);
-            
+            ResolveService ResolveService1 = new ResolveService(container);
+            ResolveService ResolveService2 = new ResolveService(container);
             ViewBag.serviceTime1 = s1.Service1(); 
             ViewBag.serviceTime2 = s2.Service2(); 
            
